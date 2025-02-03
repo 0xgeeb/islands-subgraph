674e5640172c1af60303a921ea89d7b3809a2581
0xgeeb <0xgeeb@gmail.com>
2025-02-03 09:12:04 -0500

    tried to fix error and rebuilt

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,7 +6,6 @@ import { IslandDepositor } from '../generated/schema'
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 export function handleTransfer(ev: TransferEvent): void {
-  let depositor: IslandDepositor | null
   const from = ev.params.from.toHex()
   const to = ev.params.to.toHex()
   const amt: BigInt = ev.params.amount
@@ -16,14 +15,20 @@ export function handleTransfer(ev: TransferEvent): void {
   const totalIslandSupply: BigInt = island.totalSupply()
   const balance0PerIsland = balances[0].div(totalIslandSupply)
   const balance1PerIsland = balances[1].div(totalIslandSupply)
-
+  
   if(from != ZERO_ADDRESS) {
+    let prevIslandAmt: BigInt
+    let depositor: IslandDepositor | null
     depositor = IslandDepositor.load(from)
     if(!depositor) {
       depositor = new IslandDepositor(from)
+      prevIslandAmt = BigInt.fromI32(0)
+    }
+    else {
+      prevIslandAmt = depositor.islandAmt
     }
     
-    const prevIslandAmt: BigInt = depositor.islandAmt
+    prevIslandAmt = depositor.islandAmt
     depositor.address = from
     depositor.islandAmt = prevIslandAmt.minus(amt)
     depositor.dtAmt = (prevIslandAmt.minus(amt)).times(balance0PerIsland)
@@ -32,12 +37,17 @@ export function handleTransfer(ev: TransferEvent): void {
   }
 
   if(to != ZERO_ADDRESS) {
+    let prevIslandAmt: BigInt
+    let depositor: IslandDepositor | null
     depositor = IslandDepositor.load(to)
     if(!depositor) {
       depositor = new IslandDepositor(to)
+      prevIslandAmt = BigInt.fromI32(0)
+    }
+    else {
+      prevIslandAmt = depositor.islandAmt
     }
 
-    const prevIslandAmt: BigInt = depositor.islandAmt
     depositor.address = to
     depositor.islandAmt = prevIslandAmt.plus(amt)
     depositor.dtAmt = (prevIslandAmt.plus(amt)).times(balance0PerIsland)
